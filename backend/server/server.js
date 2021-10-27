75faf3760edb7f8326347c0bc5dfa1ffc875eec8
Derth Adams <derth@me.com>
2021-10-27 16:20:37 -0700

    Split project into frontend and backend directories with Express server backend

---
+++ backend/server/server.js
@@ -0,0 +1,17 @@
+const config = require("./config.js");
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+
+app.set('port', process.argv[2]);
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
