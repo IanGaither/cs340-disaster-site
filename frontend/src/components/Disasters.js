b56ef60661ca0b84ea6406923943aa8bf472818d
Derth Adams <derth@me.com>
2021-11-04 16:53:44 -0700

    Create Instructions component with edits for new hover functionality, insert it on every table page.

--- frontend/src/components/Disasters.js
+++ frontend/src/components/Disasters.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "react-bootstrap/Card"
+import Instructions from "../Instructions"
 import TableComponent from "../TableComponent";
 
 class Disasters extends React.Component {
@@ -10,15 +10,7 @@ class Disasters extends React.Component {
                 <div className="container">
                     <h1>Disaster Events</h1>
                     <h6>This page is for viewing, editing, adding, and deleting rows to the Disasters Table.</h6>
-                    <Card className="mb-4">
-                        <Card.Body className="Body">
-                            <Card.Text className="Text">
-                                <p className="mb-0">To edit or delete a row, click on it and "Edit" and "Delete" buttons will appear.</p>
-                                <p className="mb-0">If you click the "Edit" button, the row will become editable and "Save" and "Cancel will appear.</p>
-                                <p className="mb-0">To add a row, click the "+" button below the table and a form will appear for adding new rows.</p>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Instructions />
                 </div>
                 <TableComponent source={"disasters"} />
             </div>
