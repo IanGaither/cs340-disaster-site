6ab01adedc1c17bf0b4cbac8de71358e17b3790b
Derth Adams <derth@me.com>
2021-11-16 11:30:47 -0800

    Eliminate nested <p> elements in Instructions to stop DOM warning

--- frontend/src/Instructions.js
+++ frontend/src/Instructions.js
@@ -2,16 +2,16 @@ import Card from "react-bootstrap/Card";
 import React from "react";
 
 
-function Instructions(props) {
+function Instructions() {
     return (
         <Card className="mb-4">
             <Card.Body className="Body">
-                <Card.Text className="Text">
+                {/*<Card.Text className="Text">*/}
                     <p className="mb-0">To edit or delete a row, hover over it or tap on it and "Edit" and "Delete" buttons will appear.</p>
                     <p className="mb-0">If you click the "Edit" button, the row will become editable and "Save" and "Cancel will appear.</p>
                     <p className="mb-0">To add a row, click the "+" button below the table and a form will appear for adding new rows.</p>
                     <p className="mb-0">Once a row is open for editing, you must save it or cancel before editing or adding any other rows.</p>
-                </Card.Text>
+                {/*</Card.Text>*/}
             </Card.Body>
         </Card>
     )
